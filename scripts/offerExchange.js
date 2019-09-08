a71edc8bd5f5c0d71c78473fd8750614c66b9c8e
VilemJ <vilemj@kentico.com>
2019-09-08 15:03:48 +0200

    Add server-side signalR.

--- scripts/offerExchange.js
+++ scripts/offerExchange.js
@@ -1,29 +1,31 @@
-const randomstring = require("randomstring");
+const randomstring = require('randomstring');
 const bodyParser = require('body-parser');
+const SignalRJS = require('signalrjs');
 
-const offers = {};
+const registerHub = (signalR, hubName) => {
+  signalR.hub(hubName, {
+    sendMessage: (senderId, messageType, data) => {
+      console.log(`New message on hub '${hubName}' from sender '${senderId}'`);
+      console.log('MessageType:' + messageType);
+      console.log('Message data:' + JSON.stringify(data));
+      console.log('Message END');
+      console.log('');
+
+      this.clients.all.invoke('receiveMessage').withArgs([senderId, messageType, data])
+    },
+  });
+};
 
 module.exports = {
   registerOfferExchange: (app) => {
-    app.use(bodyParser.json());
+    const signalR = new SignalRJS();
 
-    app.get('/offer/:password', (req, res) => {
-      const offer = offers[req.param.password];
-      if (offer) {
-        res.send(JSON.stringify(offer));
-      }
-      else {
-        res.send(JSON.stringify({ message: 'No offer found for this password.' }));
-        res.sendStatus(404);
-      }
-      res.end();
-    });
+    app.use(bodyParser.json());
+    app.use(signalR.createListener());
 
     app.post('/offer', (req, res) => {
       const password = randomstring.generate(8);
-      const offer = req.body;
-      offers[password] = offer;
-
+      registerHub(signalR, password);
       res.send(JSON.stringify({ password }));
       res.end();
     });
